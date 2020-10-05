9db12bacb12b97b03bbeb1b3a0033cf842bd9d68
Dmitri <dmitri2@pdx.edu>
2020-10-04 21:37:52 -0700

    Modify network for 100 bit vectors

--- GenerateRandomBitVectors.py
+++ GenerateRandomBitVectors.py
@@ -1,19 +1,29 @@
 import numpy as np
 import random
 
-vectorLength = 10
-numberOfVectors = 100
+nodesInColumn = 10
+columns = 10
+numberOfVectors = 15
 allVectors = []
 
-file = open("trainVectors.txt", "w")
+file = open("trainVectors100.txt", "w")
 
-for i in range(0, numberOfVectors):
-    vector = np.random.randint(1, size=vectorLength)
-    vector[random.randint(0, 9)] = 1
-    allVectors.append(vector)
+# For the amount of vectors we want to generate
+for k in range(0, numberOfVectors):
+    colVector = np.array([])
+    # Loop through each "column"
+    for i in range(0, columns):
+        # Loop through and generate each column vector
+        nodes = np.random.randint(1, size=nodesInColumn)
+        nodes[random.randint(0, 9)] = 1
+        print nodes
+        colVector = np.append(colVector, nodes).astype(np.int) #.append(nodes)
+
+
+    allVectors.append(colVector.tolist())
 
     # Write out each vector to file
-    for index, int in enumerate(vector):
+    for index, int in enumerate(colVector):
         if(index != 0):
             file.write(",")
         file.write(str(int))
