7d91cf39a896d1f117ca47ec2641ab4206efabad
Dmitri <dmitri2@pdx.edu>
2020-10-04 21:41:15 -0700

    Update and bound equations

--- Neuron.py
+++ Neuron.py
@@ -11,13 +11,16 @@ class Neuron:
         self.bias = 0
         self.probability = initialProbability
         self.previousProbability = 0
+        self.connectedProbabilities = []
         self.beta = 0
         self.numOfInputs = numOfInputs
-        self.inputs = []
+        self.input = 0
         self.connections = np.array([])
         self.isWinningNode = False
         self.connectionWeights = np.array([])
         self.weights = np.random.uniform(low=-0.25, high=0.25, size=(numOfInputs))
+        self.tau = 0.9
+        # print("Initialized prop to: "+str(initialProbability))
 
     def isActivated(self):
         return True if(self.value > 0.5) else False
@@ -26,7 +29,11 @@ class Neuron:
         self.weights = np.random.uniform(low=-0.25, high=0.25, size=(self.numOfInputs))
 
     def reinitializeConWeightMatrix(self):
+        ## Reinitialize the connection weight matrix
         self.connectionWeights = np.random.uniform(low=-0.25, high=0.25, size=(len(self.connections)))
+
+        ## Reinitialize the connected prob array
+        self.connectedProbabilities =  np.repeat(0.95, len(self.connections))
 
     def addConnectionFrom(self, otherNode):
         self.connections = np.append(self.connections, otherNode)
@@ -43,26 +50,33 @@ class Neuron:
         self.weights -= abs(self.weights * percent)
 
 
-    # Function that calculates the activation of the neuron
+    # Function that calculates the INITIAL activation of the neuron
     # Inputs:
     #     - input values
     #     - weights
     # Output:
     #     - node activation value
+    def calculate(self, input):
+        self.input = input
 
-    def calculate(self, inputs):
-
-        self.inputs = inputs
-
-        # Equation 1
-        # Following formula #1 from the Lansner paper
-        self.beta = math.log(self.probability, 10)
+        # ## Equation 1 - update probability for self
+        # changeInProb = (input - self.probability) / self.tau
+        #
+        # self.probability += changeInProb
 
         # Calculate the node activation (should get a single value)
         # Following formula #5 from Lansner paper
-        activation = np.dot(inputs, self.weights) + self.beta
+        # activation = sum(np.dot(input, self.weights))/len(self.weights)
+        activation = sum(np.dot(input, self.weights)) + self.bias
+
+        ## Take average of activation
         self.value = activation
 
+
+
+
+
+    ## Think of this as EQUATION 5
 
     # Function that augments the node's activation by computing the connection weights
     # Inputs:
@@ -70,11 +84,43 @@ class Neuron:
     # Output:
     #     - new node activation for self
     def calculateConnectedNodes(self):
+
+        if (self.probability <= 0):
+            self.probability = 0.00000000000000000000000001
+
+        # Equation 3 (Lansner)
+        self.bias = math.log(self.probability, 10)
+
         # For each connected node, multiple the other node's value by a internally stored weight
         # print("Initial value: "+str(self.value))
         for index, node in enumerate(self.connections):
+
+            ## Update the co-activation probability - EQUATION #2
+            newConnProb = ((self.input*node.input) - self.connectedProbabilities[index])
+            if (newConnProb == 0):
+                newConnProb = 0.00000000000000000000000001
+
+            if (newConnProb > 1e20):
+                newConnProb = 1e20
+
+            self.connectedProbabilities[index] += newConnProb / self.tau
+
+            self.value = 0
+
+            # Equation 5 - taking sum of unit's activity * connected weights
             self.value += node.value * self.connectionWeights[index]
 
+        # Equation 5 (support value being calculated with bias)
+        self.value += self.bias
+
+        ## ASK HERE
+        ## Taking sigmoid ??? Otherwise accelerates away
+        self.value = self.sigmoidOfValue(self.value)
+
+
+    def sigmoidOfValue(self, value):
+        sigmoid = 1 / (1 + math.exp(-value))
+        return sigmoid
 
     # Function to update the weights that the node has control over
     # Inputs:
@@ -83,21 +129,43 @@ class Neuron:
     # Output:
     #     - new node weights for self
     def updateWeights(self, target):
-        # Tau
-        tau = 1.5
+
+        # Equation 1
+        ## Calculate own
+        self.previousProbability = self.probability
+        changeInProb = (target - self.probability)/self.tau
+        self.probability = self.probability + changeInProb
+
+        # print("My prob: " + str(self.probability) + " | change: " + str(changeInProb)+ " | old: " + str(self.previousProbability ))
+
+        if(self.probability <= 0):
+            self.probability = 0.00000000000000000000000001
+
         # Doing logs in base 10
         for index, weight in enumerate(self.weights):
             # New weight value = log( input - e^(-1 / tau) * (input - a ^ old weight value)
-            # Where a is a the log base - I chose 10
-            a = 10
 
-            input = target
-            # input = np.mean(self.inputs)
+            # Verify the connected probability is not 0
+            if (self.connectedProbabilities[index] <= 0):
+                self.connectedProbabilities[index] = 0.00000000000000000000000001
 
-            #using target as input??
+            # print(self.connectedProbabilities[index])
 
-            self.weights[index] = math.log((input - (math.exp(-1/tau)*(input-a**self.weights[index]))), a)
-            # print("New weight: "+str(self.weights[index]))
+            # New weight update rule
+            # print("Updating weight from: " + str(self.weights[index]))
+            # print("Taking log of:" +str(self.connectedProbabilities[index]/(self.probability * self.connections[index].probability)))
+            #
+            self.weights[index] = math.log(self.connectedProbabilities[index]/(self.probability * self.connections[index].probability), 10)
+            # print("To : " + str(self.weights[index]))
+
+
+
+    # Update the interconnected nodes
+    def updateConnectedProbabilities(self):
+
+        for index, connNode in enumerate(self.connections):
+            if(connNode.value > 0.5 and self.value > 0.5):
+                self.connectedProbabilities[index] *= 1.3
 
 
 
